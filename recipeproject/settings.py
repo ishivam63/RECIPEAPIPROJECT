56e661980c54fac9d4c8901b634d13591611068c
ishivam63 <shivamdsingh222@gmail.com>
2026-04-18 22:25:16 +0530

    vercel deploy setup

--- recipeproject/settings.py
+++ recipeproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*#4(*z#0h3df&qv=x7dj&e0ds4a+r+y**4s1!+ahbolplz+&jc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
